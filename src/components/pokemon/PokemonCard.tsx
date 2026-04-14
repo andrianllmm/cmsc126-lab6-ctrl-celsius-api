badc672625e23023e801ba3f9540b3dd872d0c87
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-04-14 18:06:32 +0800

    chore: add links to card

--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -3,6 +3,7 @@ import PokemonTypeBadge from "./PokemonTypeBadge";
 import PokemonSprite from "./PokemonSprite";
 import { getPokemonSprite } from "@/utils/getPokemonSprite";
 import { Card, CardContent } from "@/components/ui/8bit/card";
+import { Link } from "react-router";
 
 type Props = {
   pokemon: PokemonDetail;
@@ -10,17 +11,31 @@ type Props = {
 
 const PokemonCard = ({ pokemon }: Props) => {
   return (
-    <Card>
-      <CardContent>
-        <PokemonSprite src={getPokemonSprite(pokemon)} alt={pokemon.name} />
-        <h3 className="text-center font-semibold capitalize">{pokemon.name}</h3>
-        <div className="flex gap-1 justify-center mt-2 flex-wrap">
-          {pokemon.types.map((t) => (
-            <PokemonTypeBadge key={t.type.name} type={t.type.name} />
-          ))}
-        </div>
-      </CardContent>
-    </Card>
+    <Link to={`/pokemon/${pokemon.name}`} className="block group">
+      <Card
+        className="
+          transition-all duration-200
+          hover:-translate-y-1.5
+          cursor-pointer
+        "
+      >
+        <CardContent>
+          <PokemonSprite
+            src={getPokemonSprite(pokemon)}
+            alt={pokemon.name}
+            className="mx-auto transition-all duration-200 group-hover:scale-110 group-hover:rotate-3 group-hover:brightness-110"
+          />
+          <h3 className="text-center font-semibold capitalize">
+            {pokemon.name}
+          </h3>
+          <div className="flex gap-1 justify-center mt-2 flex-wrap">
+            {pokemon.types.map((t) => (
+              <PokemonTypeBadge key={t.type.name} type={t.type.name} />
+            ))}
+          </div>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
 
--- src/components/pokemon/PokemonSprite.tsx
+++ src/components/pokemon/PokemonSprite.tsx
@@ -1,14 +1,17 @@
+import { cn } from "@/lib/utils";
+
 type Props = {
   src: string | null;
   alt: string;
+  className?: string;
 };
 
-const PokemonSprite = ({ src, alt }: Props) => {
+const PokemonSprite = ({ src, alt, className }: Props) => {
   return (
     <img
       src={src ?? ""}
       alt={alt}
-      className="w-24 h-24 mx-auto"
+      className={cn("w-24 h-24", className)}
       loading="lazy"
     />
   );
